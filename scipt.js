a6f73859db9adaa5078f42876522a62f70fdf69f
BradiLugembaTUD <bradi.lugemba@gmail.com>
2025-03-13 13:52:08 +0000

    adding style through bootsrap

--- scipt.js
+++ scipt.js
@@ -1,12 +1,10 @@
 const input = document.getElementById("input");
-const taskList_P1 = document.getElementById("task-list_P1")
-const taskList_P2 = document.getElementById("task-list_P2")
-const taskList_P3 = document.getElementById("task-list_P3")
+
 const option = document.getElementById("priority")
 
 function addTask() {
     const task = input.value
-    const priority = option.value
+    const taskPriority = option.value
 
     console.log(priority)
     if(task.trim() === ""){
@@ -19,20 +17,10 @@ function addTask() {
     taskText.classList.add("task-text");
     taskText.textContent = task;
     taskItem.appendChild(taskText);
-
-    if (priority === "1") {
-        taskList_P1.appendChild(taskItem);
-    }
-    else if (priority === "2"){
-        taskList_P2.appendChild(taskItem);
-    }
-    else{
-        taskList_P3.appendChild(taskItem);
-    }
-    
+    const taskList = document.getElementById("task-list-" + taskPriority)
+    taskList.appendChild(taskItem);
 
 
     input.value = "";
-    
 
 }
