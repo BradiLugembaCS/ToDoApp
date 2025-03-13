f4cf639593ac43931a52dd0d966ca7b32bce63f2
BradiLugembaTUD <bradi.lugemba@gmail.com>
2025-03-13 11:55:18 +0000

    adding task under each priority

--- scipt.js
+++ scipt.js
@@ -1,12 +1,18 @@
 const input = document.getElementById("input");
-const taskList = document.getElementById("task-list")
+const taskList_P1 = document.getElementById("task-list_P1")
+const taskList_P2 = document.getElementById("task-list_P2")
+const taskList_P3 = document.getElementById("task-list_P3")
+const option = document.getElementById("priority")
 
 function addTask() {
     const task = input.value
+    const priority = option.value
 
+    console.log(priority)
     if(task.trim() === ""){
         return;
     }
+
 
     const taskItem = document.createElement("li");
     const taskText = document.createElement("span");
@@ -14,7 +20,19 @@ function addTask() {
     taskText.textContent = task;
     taskItem.appendChild(taskText);
 
-    taskList.appendChild(taskItem);
+    if (priority === "1") {
+        taskList_P1.appendChild(taskItem);
+    }
+    else if (priority === "2"){
+        taskList_P2.appendChild(taskItem);
+    }
+    else{
+        taskList_P3.appendChild(taskItem);
+    }
+    
+
+
     input.value = "";
+    
 
 }
